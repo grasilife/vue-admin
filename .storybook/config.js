bfd8c093b77589bdc8f05f5312aa1835f9295610
grasilife <fu.guo.qiang@qq.com>
2019-04-21 21:57:43 +0800

    添加我的主题

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,19 +1,16 @@
-import {
-  configure
-} from '@storybook/vue';
-import {
-  themes
-} from '@storybook/theming';
+import { configure } from '@storybook/vue'
+import { themes } from '@storybook/theming'
+import myTheme from './myTheme'
 
 function loadStories() {
-  require('../stories/index.js');
+  require('../stories/index.js')
   // You can require as many stories as you need.
+  // Option defaults.
+  addParameters({
+    options: {
+      theme: myTheme
+    }
+  })
 }
 
-configure(loadStories, module);
-// Option defaults.
-addParameters({
-  options: {
-    theme: themes.dark,
-  },
-});
+configure(loadStories, module)
